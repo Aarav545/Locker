7e9f3669299482567089a02a7f68304a17e746bb
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2020-11-07 17:22:04 -0500

    google auth

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,58 @@
-import Head from 'next/head'
-import { Page, Text, Card, Note, Code, Spacer } from '@geist-ui/react'
+import { Page, Button, Spacer } from "@geist-ui/react";
+import * as Icon from '@geist-ui/react-icons'
+import { useState } from "react";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
 
 export default function Home() {
+  const [isLoggedIn, setLoggedIn] = useState(false)
+
+  const clientID = '670037657499-5ak3trgnr5b1lgkj0kod8hh5bpo2qvi8.apps.googleusercontent.com'
+
+  const onSuccess = res => {
+    setLoggedIn(true)
+    console.log("[Login Success]:", res)
+  }
+
+  const onFailure = res => {
+    setLoggedIn(false)
+    console.log("[Login Failed]:", res)
+  }
+
+  const onLogoutSuccess = () => {
+    setLoggedIn(false)
+    console.log("Logged Out!")
+  }
+
   return (
-    <Page>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Text h1>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </Text>
-      <Card>
-        hello, world. I am using <Code>@geist-ui/react</Code> !
-      </Card>
-      <Spacer y={1.5} />
-      <Card shadow>
-        <Note type="success">This note details something important.</Note>
-      </Card>
+    <>
+    <Page style={{paddingTop: '56px', paddingBottom: '56px'}} size="mini">
+      <div>
+        <h1>locker</h1>
+        <div>Note taking made for students.</div>
+      </div>
+      <Spacer y={2} />
+      {isLoggedIn ? <GoogleLogout 
+          clientId={clientID}
+          onLogoutSuccess={onLogoutSuccess}
+          render={renderProps => (
+            <Button onClick={renderProps.onClick} disabled={renderProps.disabled}>
+              Logout
+            </Button>
+          )}
+      /> : <GoogleLogin 
+        clientId={clientID}
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy="single_host_origin"
+        isSignedIn={false}
+        render={renderProps => (
+          <Button onClick={renderProps.onClick} disabled={renderProps.disabled}>
+            Sign in with Google
+          </Button>
+        )}
+      />
+      }
     </Page>
+    </>
   )
 }
