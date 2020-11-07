ecaa2306eebb3dccacbe11a73bb0467cb808daeb
Aaravsinh Solanki <aaravsolanki1704@gmail.com>
2020-11-07 17:50:33 -0500

    API stuff done

---
+++ pages/api/create.js
@@ -0,0 +1,47 @@
+import cass from "cassandra-driver"
+
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({
+    cloud: { secureConnectBundle: '../../driver/secure-connect-bucket.zip' },
+    credentials: { username: 'nabeel', password: 'X6-]7Pzq^B9>&_L@' },
+    keyspace: 'data'
+  });
+
+function createTable(){
+    const query = "CREATE TABLE IF NOT EXISTS data.users \
+    (lastname text PRIMARY KEY, email text, firstname text);";
+    return client.execute(query);
+  }
+
+function insertUser(lastname, email, firstname) {
+    // TO DO: execute a simple statement that inserts one user into the table
+    const insert = 'INSERT INTO users (lastname, email, firstname) VALUES (?,?,?,?,?)';
+    const params = [ lastname, email, firstname ];
+    return client.execute(insert, params);
+  }
+
+function selectUser(lastname) {
+    // TO DO: execute a simple statement that retrieves one user from the table
+    const select = 'SELECT firstname, age FROM users WHERE lastname = :lastname';
+    const params = [ lastname ] ;
+    return client.execute(select, params);
+  }
+
+async function example() {
+    await client.connect();
+    await insertUser('Jones', 35, 'Austin', 'bob@example.com', 'Bob');
+    const rs1 = await selectUser('Jones');
+    const user1 = rs1.first();
+    if (user1) {
+      console.log("name = %s, age = %d", user1.firstname, user1.age);
+    } else {
+      console.log("No results");
+    }
+    await updateUser(36, 'Jones');
+    const rs2 = await selectUser('Jones');
+    const user2 = rs2.first();
+    if (user2) {
+      console.log("name = %s, age = %d", user2.firstname, user2
+    }
+}
