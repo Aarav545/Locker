c0052b57e918c2c11cee9e48766be5a37c142dde
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2020-11-08 09:48:09 -0500

    sharing

--- components/dashboard.js
+++ components/dashboard.js
@@ -3,6 +3,7 @@ import * as Icon from '@geist-ui/react-icons'
 import { useEffect, useState } from 'react'
 import styles from '../styles/dashboard.module.css'
 import showdown, { Converter } from 'showdown'
+import Sketchpad from '../components/sketchpad'
 
 export default function Dashboard ({ userData }) {
   const [rawData, setRawData] = useState([])
@@ -12,6 +13,12 @@ export default function Dashboard ({ userData }) {
   const [noteTitle, setNoteTitle] = useState('')
   const [noteContent, setNoteContent] = useState('')
   const [noteContentPreview, setNoteContentPreview] = useState('')
+
+  const [isINoteModalOpen, setINoteModalOpen] = useState(false)
+  const [iNoteTitle, setINoteTitle] = useState('')
+  const [iNoteContent, setINoteContent] = useState('')
+
+  const [sharing, setSharing] = useState('')
 
   useEffect(() => {
     const req = fetch('/api/notes', {
@@ -49,11 +56,6 @@ export default function Dashboard ({ userData }) {
     )))
   }
 
-  function markdownToHTML (data) {
-    const converter = new showdown.Converter()
-    return converter.makeHtml(data)
-  }
-
   useEffect(() => {
     setNoteContentPreview(markdownToHTML(noteContent))
   }, [noteContent])
@@ -85,8 +87,80 @@ export default function Dashboard ({ userData }) {
     })
   }
 
+  function showNote (data) {
+    setINoteModalOpen(true)
+    setINoteTitle(data.title)
+    setINoteContent(data.desc)
+  }
+
+  function closeShowNote () {
+    setINoteModalOpen(false)
+  }
+
+  const Note = (data) => {
+    data = data.data
+
+    return (
+      <Card onClick={() => showNote(data)} className={styles.note} width="400px">
+          <h4>{data.title}</h4>
+          <p dangerouslySetInnerHTML={{ __html: markdownToHTML(data.desc) }} />
+          <div>
+          {/* {
+              data.tags.map((tag, key) => (
+                  <Badge key={key} className={styles.badge}>{tag}</Badge>
+              ))
+          } */}
+          </div>
+      </Card>
+    )
+  }
+
+  function markdownToHTML (data) {
+    const converter = new showdown.Converter()
+    return converter.makeHtml(data)
+  }
+
+  function sharingHandler () {
+    const req = fetch('/api/sharing', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        ...userData,
+        partner: sharing,
+        noteTitle: iNoteTitle,
+        noteContent: iNoteContent
+      })
+    })
+    console.log('Sharing...')
+    req.then(data => {
+      data.text().then(response => {
+        setSharing('')
+        alert('Shared Successfully')
+      })
+    })
+  }
+
   return (
       <div>
+
+        <Modal width="100%" open={isINoteModalOpen} onClose={() => setINoteModalOpen(false)}>
+            <Modal.Title>{iNoteTitle}</Modal.Title>
+            <Modal.Content>
+                <h4>Share:</h4>
+                <Input placeholder="Type in partner's email..." width="100%" value={sharing} onChange={event => setSharing(event.target.value)}></Input>
+                <Spacer x={1} />
+                <Button onClick={sharingHandler} size="small">Share</Button>
+                <Spacer y={1} />
+                <h4>Content:</h4>
+                <Textarea width="100%" minHeight="250px" value={iNoteContent} onChange={event => setINoteContent(event.target.value)} />
+                <Spacer y={1} />
+                <div dangerouslySetInnerHTML={{ __html: markdownToHTML(iNoteContent) }} />
+            </Modal.Content>
+            <Modal.Action passive onClick={() => closeShowNote(false)}>Close</Modal.Action>
+        </Modal>
 
         <Modal width="100%" open={isNoteModalOpen} onClose={() => setNoteModalOpen(false)}>
             <Modal.Title>Create a new note</Modal.Title>
@@ -102,10 +176,11 @@ export default function Dashboard ({ userData }) {
                             value={noteContent}
                             width="100%"
                             minHeight="350px"
-                            placeholder="Note title..."
+                            placeholder="Note content..."
                         >
                         </Textarea>
                         <Spacer y={1} />
+                        {/* <Sketchpad /> */}
                         {/* <h4>Files:</h4>
                         <Input width="100%" type="file"></Input> */}
                     </div>
@@ -150,28 +225,5 @@ export default function Dashboard ({ userData }) {
   //     <Textarea width="100%" />
   //     <Button type="secondary" style={{ marginTop: '12px', float: 'right' }}>Upload</Button>
   // </div>
-  )
-}
-
-const Note = data => {
-  data = data.data
-
-  function markdownToHTML (data) {
-    const converter = new showdown.Converter()
-    return converter.makeHtml(data)
-  }
-
-  return (
-    <Card className={styles.note} width="400px">
-        <h4>{data.title}</h4>
-        <p dangerouslySetInnerHTML={{ __html: markdownToHTML(data.desc) }} />
-        <div>
-        {/* {
-            data.tags.map((tag, key) => (
-                <Badge key={key} className={styles.badge}>{tag}</Badge>
-            ))
-        } */}
-        </div>
-    </Card>
   )
 }
---
+++ pages/api/sharing.js
@@ -0,0 +1,86 @@
+// Datastax
+
+import cassandra from 'cassandra-driver'
+import root from 'app-root-path'
+import monk from 'monk'
+
+const client = new cassandra.Client({
+  cloud: { secureConnectBundle: (root + '/driver/secure-connect-bucket.zip') },
+  credentials: { username: 'nabeel', password: 'X6-]7Pzq^B9>&_L@' },
+  keyspace: 'data'
+})
+
+function createTable () {
+  const query = 'CREATE TABLE IF NOT EXISTS data.users (id int PRIMARY KEY, lastname text, email text, firstname text);'
+  return client.execute(query)
+}
+
+export default function handler (req, res) {
+  if (req.method === 'POST') {
+    createTable()
+    if (req.body.googleId) {
+      const user = req.body
+      if (user.googleId) {
+        _shareNotes(res, user.partner, user.noteTitle, user.noteContent)
+      }
+    }
+  } else {
+    res.send('Method not allowed')
+  }
+}
+
+function insertUser (id, lastname, email, firstname) {
+  // TO DO: execute a simple statement that inserts one user into the table
+  const insert = 'INSERT INTO users (id, lastname, email, firstname) VALUES (?,?,?,?,?)'
+  const params = [id, lastname, email, firstname]
+  return client.execute(insert, params)
+}
+
+function selectUser (lastname) {
+  // TO DO: execute a simple statement that retrieves one user from the table
+  const select = 'SELECT firstname, age FROM users WHERE lastname = :lastname'
+  const params = [lastname]
+  return client.execute(select, params)
+}
+
+async function writeDoc () {
+  await client.connect()
+  // await insertUser(user, id, lname, email, fname)
+  const rs1 = await selectUser('Jones')
+  const user1 = rs1.first()
+
+  if (user1) {
+    console.log('name = %s, age = %d', user1.firstname, user1.age)
+  } else {
+    console.log('No results')
+  }
+
+  // await updateUser(36, 'Jones')
+
+  const rs2 = await selectUser('Jones')
+  const user2 = rs2.first()
+
+  if (user2) {
+    console.log('name = %s, age = %d', user2.firstname, user2)
+  }
+}
+
+const db = monk('mongodb+srv://na-admin:tYPhfsVwT63qFuuy@bucket1.qpofb.mongodb.net/locker')
+const col = db.get('notes')
+const users = db.get('users')
+
+function _shareNotes (res, partner, noteTitle, noteDesc) {
+  col.find({ noteTitle })
+    .then(() => {
+      users.findOne({ email: partner }).then(response => {
+        if (response.id) {
+          col.insert({
+            id: response.id,
+            email: partner,
+            title: noteTitle,
+            desc: noteDesc
+          })
+        }
+      })
+    })
+}
